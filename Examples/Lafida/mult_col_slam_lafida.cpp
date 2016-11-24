2fd8b202e30616a449b22ef8ac19305e46b1dfe1
Vandeportaele Bertrand <bvandepo@laas.fr>
2016-11-24 23:01:16 +0100

    removed synchronization with a desired framerate in the example program

--- Examples/Lafida/mult_col_slam_lafida.cpp
+++ Examples/Lafida/mult_col_slam_lafida.cpp
@@ -138,9 +138,9 @@ int main(int argc, char **argv)
 			T = tframe - timestamps[ni - 1];
 		//std::this_thread::sleep_for(std::chrono::milliseconds(30));
 
-		if (ttrack < T)
-			std::this_thread::sleep_for(std::chrono::milliseconds(
-			static_cast<long>((T - ttrack))));
+		//if (ttrack < T)
+		//	std::this_thread::sleep_for(std::chrono::milliseconds(
+		//	static_cast<long>((T - ttrack))));
 	}
 
 	// Stop all threads
